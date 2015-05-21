b93dd9c5420399e4f48766898b3748225f3baec7
chancejin <woo.yeunjin@gmail.com>
2015-05-21 21:17:57 +0900

    4fragments_add_ing

--- app/src/main/java/com/example/ttong/Activity/MainActivity.java
+++ app/src/main/java/com/example/ttong/Activity/MainActivity.java
@@ -1,39 +1,27 @@
 package com.example.ttong.Activity;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
+import android.app.ActionBar;
+import android.app.ActionBar.*;
 
 import com.example.ttong.Fragment.DialFragment;
-import com.example.ttong.Fragment.TtongFragment;
-import com.example.ttong.Fragment.HistoryFragment;
-import com.example.ttong.Fragment.ContactFragment;
 import com.example.ttong.Listener.TabListener;
 import com.example.ttong.R;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
-        final ActionBar actionBar = getSupportActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        ActionBar abar = getActionBar();
+        abar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-        actionBar.addTab(actionBar.newTab().setText("Dial").setTabListener(new TabListener<DialFragment>(this,"dial",DialFragment.class)));
-
-        actionBar.addTab(actionBar.newTab().setText("TTong").setTabListener(new TabListener<TtongFragment>(this,"ttong",TtongFragment.class)));
-
-        actionBar.addTab(actionBar.newTab().setText("History").setTabListener(new TabListener<HistoryFragment>(this,"dial",HistoryFragment.class)));
-
-        actionBar.addTab(actionBar.newTab().setText("Contact").setTabListener(new TabListener<ContactFragment>(this,"dial",ContactFragment.class)));
-
-    }
-
-    protected void onSaveInstanceState(Bundle outState){
-        super.onSaveInstanceState(outState);
-        outState.putInt("selectedTab",getActionBar().getSelectedNavigationIndex());
+        Tab dialTab = abar.newTab();
+        dialTab.setText("dial");
+        dialTab.setTabListener(new ProductTabListener(this,DialFragment.class.getName()));
+        abar.addTab(dialTab);
     }
 }
---
+++ app/src/main/java/com/example/ttong/Listener/ProductTabListener.java
@@ -0,0 +1,7 @@
+package com.example.ttong.Listener;
+
+/**
+ * Created by cse109 on 2015-05-21.
+ */
+public class ProductTabListener {
+}
