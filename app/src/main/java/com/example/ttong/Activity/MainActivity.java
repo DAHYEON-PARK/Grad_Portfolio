5da38ed2b01b1e0d87fad6acb5ae58adc8079663
chancejin <woo.yeunjin@gmail.com>
2015-05-21 21:30:39 +0900

    tab!!!!!!!!!!!

--- app/src/main/java/com/example/ttong/Activity/MainActivity.java
+++ app/src/main/java/com/example/ttong/Activity/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.ttong.Activity;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.app.ActionBar;
 import android.app.ActionBar.*;
 
 import com.example.ttong.Fragment.DialFragment;
-import com.example.ttong.Listener.TabListener;
 import com.example.ttong.R;
 
 public class MainActivity extends Activity {
@@ -21,7 +22,34 @@ public class MainActivity extends Activity {
 
         Tab dialTab = abar.newTab();
         dialTab.setText("dial");
-        dialTab.setTabListener(new ProductTabListener(this,DialFragment.class.getName()));
+        dialTab.setTabListener(new ProductTabListener(this, DialFragment.class.getName()));
         abar.addTab(dialTab);
     }
+
+    private class ProductTabListener implements ActionBar.TabListener{
+        private Fragment mFragment;
+        private final Activity mActivity;
+        private final String mFragName;
+
+        public ProductTabListener(Activity activity, String fragName){
+            mActivity = activity;
+            mFragName = fragName;
+        }
+
+        public void onTabReselected(Tab tab, FragmentTransaction arg1){
+        }
+
+        // tab selected
+        public void onTabSelected(Tab tab, FragmentTransaction ft){
+            mFragment = Fragment.instantiate(mActivity,mFragName);
+            ft.add(android.R.id.content, mFragment);
+        }
+
+        // tab unselected
+        public void onTabUnselected(Tab tab, FragmentTransaction ft){
+            ft.remove(mFragment);
+            mFragment = null;
+        }
+    }
 }
+
--- app/src/main/java/com/example/ttong/Listener/ProductTabListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.ttong.Listener;
-
-/**
- * Created by cse109 on 2015-05-21.
- */
-public class ProductTabListener {
-}
